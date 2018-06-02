fc187414e456365c83f6c25265675ec1b5a2ce06
thangn <thangn.1411@gmail.com>
2018-06-02 09:53:03 +0700

    add build

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const MinifyPlugin = require("babel-minify-webpack-plugin");
+
+let common = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      }
+    ]
+  },
+  node: {
+    fs: 'empty',
+    'child_process': 'empty'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['lib'])
+  ]
+}
+
+if (process.env.NODE_ENV === 'development') {
+  common.devtool = 'source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  common.plugins.push(new MinifyPlugin())
+}
+
+
+module.exports = function (mode) {
+  return [
+    Object.assign({}, common, {
+      target: 'node',
+      output: {
+        path: __dirname,
+        filename: './lib/index.js',
+        libraryTarget: 'umd'
+      }
+    }),
+    Object.assign({}, common, {
+      target: 'web',
+      output: {
+        path: __dirname,
+        filename: './lib/browser.js',
+        libraryTarget: 'umd',
+        library: 'Ichain' // This is the var name in browser
+      }
+    })
+  ]
+}
