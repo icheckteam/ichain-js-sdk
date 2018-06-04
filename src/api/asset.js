9e7873704de572eda7264f413a2a29bd90fb6fb2
thangn <thangn.1411@gmail.com>
2018-06-04 14:40:55 +0700

    test api asset

--- src/api/asset.js
+++ src/api/asset.js
@@ -56,4 +56,273 @@ export const sendAsset = (net, from, to, assets) => {
   return axios.post(`${endpoint}/accounts/${fromAcct.address}/send`, body);
 }
 
+/**
+ * create new an asset
+ * @param {string} net 
+ * @param {string} issuer the private key of the issuer
+ * @param {options} opts 
+ * @param {string} opts.id the id of the asset
+ * @param {string} opts.name the name of the asset
+ * @param {string} opts.company the company name 
+ * @param {string} opts.email the email of company
+ * @param {number} opts.quantity
+ */
+export const createAsset = (net, issuer, opts) => {
+  const endpoint = getAPIEndpoint(net);
+  const issuerAcc = new Key(issuer)
+  var tx = {
+    chain_id: "test",
+    sequences: [0],
+    msg: {
+      issuer: issuerAcc.address.toUpperCase(),
+      id: opts.id,
+      name: opts.name,
+      company: opts.company,
+      email: opts.email,
+    },
+  }
+  const signature = signMsg(serializeTx(tx), issuerAcc.privateKey)
+  const body = {
+    chain_id: "test",
+    sequences: [0],
+    issuer: issuerAcc.address.toUpperCase(),
+    asset_id: opts.id,
+    name: opts.name,
+    company: opts.company,
+    email: opts.email,
+    signature: signature,
+  }
+  return axios.post(`${endpoint}/assets`, body);
+}
 
+
+/**
+ * create new an asset
+ * @param {string} net 
+ * @param {string} assetId the id of the asset
+ */
+export const getAsset = (net, assetId) => {
+  const endpoint = getAPIEndpoint(net);
+  return axios.post(`${endpoint}/assets/${assetId}`, body);
+}
+
+
+/**
+ * add quantity for the asset
+ * @param {string} net 
+ * @param {string} issuer the private key of the issuer
+ * @param {object} opts 
+ * @param {string} opts.assetId
+ * @param {array}  opts.materials the list material of the asset
+ * @param {number} opts.quantity 
+ */
+export const addQuantity = (net, issuer, opts) => {
+  const endpoint = getAPIEndpoint(net);
+  const issuerAcc = new Key(issuer)
+  var tx = {
+    chain_id: "test",
+    sequences: [0],
+    msg: {
+      issuer: issuerAcc.address.toUpperCase(),
+      id: opts.assetId,
+      quantity: opts.quantity,
+      materials: opts.materials,
+    },
+  }
+  const signature = signMsg(serializeTx(tx), issuerAcc.privateKey)
+  const body = {
+    chain_id: "test",
+    sequences: [0],
+    issuer: issuerAcc.address.toUpperCase(),
+    quantity: opts.quantity,
+    assetId: opts.assetId,
+    materials: opts.materials,
+    signature: signature,
+  }
+  const url = `${endpoint}/assets/${opts.assetId}/add-quantity`;
+  return axios.post(url, body);
+}
+
+/**
+ * subtract quantity
+ * @param {string} net 
+ * @param {string} issuer the private key of the issuer
+ * @param {string} assetId the id of the asset
+ * @param {number} quantity the quantity to subtract
+ */
+export const subtractQuantity = (net, issuer, assetId, quantity ) => {
+  const endpoint = getAPIEndpoint(net);
+  const issuerAcc = new Key(issuer)
+  var tx = {
+    chain_id: "test",
+    sequences: [0],
+    msg: {
+      issuer: issuerAcc.address.toUpperCase(),
+      id: assetId,
+      quantity: quantity,
+    },
+  }
+  const signature = signMsg(serializeTx(tx), issuerAcc.privateKey)
+  const body = {
+    chain_id: "test",
+    sequences: [0],
+    issuer: issuerAcc.address.toUpperCase(),
+    quantity: quantity,
+    assetId: assetId,
+    signature: signature,
+  }
+  const url = `${endpoint}/assets/${assetId}/subtract-quantity`
+  return axios.post(url, body);
+}
+
+
+/**
+ * create proposal
+ * @param {string} net 
+ * @param {string} issuer the private key of the issuer
+ * @param {object} opts 
+ * @param {string} opts.assetId 
+ * @param {string} opts.recipient
+ * @param {array} opts.propertipes
+ * @param {role} opts.role
+ */
+export const createProposal = (net, issuer, opts ) => {
+  const endpoint = getAPIEndpoint(net);
+  const issuerAcc = new Key(issuer)
+  var tx = {
+    chain_id: "test",
+    sequences: [0],
+    msg: {
+      asset_id: opts.assetId,
+      issuer: issuerAcc.address.toUpperCase(),
+      recipient: opts.recipient,
+      propertipes: opts.propertipes,
+      role: opts.role,
+    },
+  }
+  const signature = signMsg(serializeTx(tx), issuer)
+  const body = {
+    chain_id: "test",
+    sequences: [0],
+
+    asset_id: opts.assetId,
+    issuer: issuerAcc.address.toUpperCase(),
+    recipient: opts.recipient,
+    propertipes: opts.propertipes,
+    role: opts.role,
+    signature: signature
+  }
+  const url = `${endpoint}/assets/${opts.assetId}/create-proposal`
+  return axios.post(url, body);
+}
+
+/**
+ * answer proposal
+ * @param {string} net 
+ * @param {string} issuer the private key of the issuer
+ * @param {object} opts 
+ * @param {string} opts.assetId 
+ * @param {string} opts.recipient
+ * @param {number} opts.response
+ */
+export const answerProposal = (net, issuer, opts ) => {
+  const endpoint = getAPIEndpoint(net);
+  const issuerAcc = new Key(issuer)
+  var tx = {
+    chain_id: "test",
+    sequences: [0],
+    msg: {
+      asset_id: opts.assetId,
+      recipient: issuerAcc.address.toUpperCase(),
+      response: opts.response,
+    },
+  }
+  const signature = signMsg(serializeTx(tx), issuer)
+  const body = {
+    chain_id: "test",
+    sequences: [0],
+
+    asset_id: opts.assetId,
+    recipient: issuerAcc.address.toUpperCase(),
+    response: opts.response,
+
+    signature: signature
+  }
+  const url = `${endpoint}/assets/${opts.assetId}/answer-proposal`
+  return axios.post(url, body);
+}
+
+
+/**
+ * revoke proposal
+ * @param {string} net 
+ * @param {string} issuer the private key of the issuer
+ * @param {object} opts 
+ * @param {string} opts.assetId 
+ * @param {string} opts.recipient
+ * @param {array} opts.propertipes
+ */
+export const revokeProposal = (net, issuer, opts ) => {
+  const endpoint = getAPIEndpoint(net);
+  const issuerAcc = new Key(issuer)
+  var tx = {
+    chain_id: "test",
+    sequences: [0],
+    msg: {
+      asset_id: opts.assetId,
+      issuer: issuerAcc.address.toUpperCase(),
+      recipient: opts.recipient,
+      response: opts.response,
+    },
+  }
+  const signature = signMsg(serializeTx(tx), issuerAcc.privateKey)
+  const body = {
+    chain_id: "test",
+    sequences: [0],
+
+    asset_id: opts.assetId,
+    issuer: issuerAcc.address.toUpperCase(),
+    recipient: opts.recipient,
+    response: opts.response,
+    
+    signature: signature
+  }
+  const url =`${endpoint}/assets/${opts.assetId}/revoke-proposal`
+  return axios.post(url, body);
+}
+
+
+
+/**
+ * update attributes
+ * @param {string} net 
+ * @param {string} issuer the private key of the issuer
+ * @param {string} assetId the id of the asset
+ * @param {array} attributes 
+ */
+export const updateAttributes = (net, issuer, assetId, attributes ) => {
+  const endpoint = getAPIEndpoint(net);
+  const issuerAcc = new Key(issuer)
+  var tx = {
+    chain_id: "test",
+    sequences: [0],
+    msg: {
+      issuer: issuerAcc.address.toUpperCase(),
+      id: assetId,
+      attributes: attributes,
+    },
+  }
+  const signature = signMsg(serializeTx(tx), issuerAcc.privateKey)
+  const body = {
+    chain_id: "test",
+    sequences: [0],
+
+    issuer: issuerAcc.address.toUpperCase(),
+    asset_id: assetId,
+    attributes: attributes,
+    
+    signature: signature
+  }
+  const url = `${endpoint}/assets/${assetId}/update-attribute`
+  return axios.post(url, body);
+}
--- test/unit/api/asset.js
+++ test/unit/api/asset.js
@@ -15,4 +15,58 @@ describe('Core API', function () {
   it("sendAsset", () => {
     return asset.sendAsset("testnet", testKeys.a.privateKey, testKeys.b.address, [{amount: 10, assetId: "1"}]);
   });
+
+  it("createAsset", () => {
+    return asset.createAsset("testnet", testKeys.a.privateKey, {
+      id: "tomato",
+      name: "Tomato",
+      quantity: 100,
+      company: "Tomato comapny",
+      email: "tomato@gmail.com"
+    });
+  });
+
+  it("addQuantity", () => {
+    return asset.addQuantity("testnet", testKeys.a.privateKey, {
+      assetId: "tomatoRetailer",
+      quantity: 100,
+      materials: [
+        { asset_id: "tomato", amount: 100}
+      ]
+    });
+  });
+
+  it("subtractQuantity", () => {
+    return asset.subtractQuantity("testnet", testKeys.a.privateKey, "tomatoRetailer", 150);
+  });
+
+  it("createProposal", () => {
+    return asset.createProposal("testnet", testKeys.a.privateKey, {
+      assetId: "tomatoRetailer",
+      recipient: testKeys.b.address,
+      propertipes: ["height", "location"],
+      role: 1,
+    });
+  });
+
+  it("answerProposal", () => {
+    return asset.answerProposal("testnet", testKeys.a.privateKey, {
+      assetId: "tomatoRetailer",
+      response: 1,
+    });
+  });
+
+  it("revokeProposal", () => {
+    return asset.revokeProposal("testnet", testKeys.a.privateKey, {
+      assetId: "tomatoRetailer",
+      recipient: testKeys.a.privateKey,
+      propertipes: ["height"],
+    });
+  });
+  it("updateAttributes", () => {
+    return asset.updateAttributes("testnet", testKeys.a.privateKey, "tomatoRetailer", [
+      {name: "weight", number_value: 100}
+    ]);
+  });
+
 })
